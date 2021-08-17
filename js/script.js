f24955f8210e444f672fe258e2d65bab9d6492a4
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-08-17 10:19:10 +0100

    adding the password toggle off and on

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-const test = require("./test");
+const show = document.getElementById('show');
 
-test('John');
+const password = document.getElementById('password')
+
+show.addEventListener('click', () => {
+
+
+    if (password.type === "password") {
+
+        password.type = "text"
+        show.innerHTML = "hide"
+    } else {
+        password.type = "password"
+        show.innerHTML = "show"
+
+    }
+
+})
